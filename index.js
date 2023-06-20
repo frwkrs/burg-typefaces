83a9a5b517d466e92dfe4e84d0dfa085e4964218
Volker Buchwald <vennnq@gmail.com>
2023-06-20 20:27:59 +0200

    correcting paths

--- index.js
+++ index.js
@@ -79,6 +79,7 @@ var upload = multer({ storage: storage });
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname));
+
 app.use('/typefaces', router);
 app.use(express.urlencoded({ extended: true }));
 // app.use(countFilejs);
